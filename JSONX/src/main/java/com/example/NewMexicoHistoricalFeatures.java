ae0b967ec586acf988db99dc0ecbd33b389cc9f9
Gunnerfront <jacob.owl.24@gmail.com>
2022-02-01 23:54:17 -0600

    All tests made

--- JSONX/src/main/java/com/example/NewMexicoHistoricalFeatures.java
+++ JSONX/src/main/java/com/example/NewMexicoHistoricalFeatures.java
@@ -1,5 +1,7 @@
 package com.example;
 
+//import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +12,16 @@ public class NewMexicoHistoricalFeatures {
   // Subtraction metric used for comparing doubles
   private static final Double EPSILON = 0.000001d;
 
+  // Constants representing miles per degree longitude or latitude
+  private static final Double MILES_PER_DEGREE_LONGITUDE = 54.6;
+  private static final Double MILES_PER_DEGREE_LATITUDE = 69.0;
+
   /**
    * Filtering method for getting a list of only the historical features with known coordinates
    *
    * @return a List container holding HistoricalFeature objects which have known coordinates
    */
-  public List<HistoricalFeature> getLocatableFeatures() {
+  public List<HistoricalFeature> findLocatableFeatures() {
     List<HistoricalFeature> locatableFeatures = new ArrayList<HistoricalFeature>();
     for (HistoricalFeature feature : features) {
       Double longitude = feature.getPropertyData().getLongitudeAsNumber();
@@ -30,16 +36,114 @@ public class NewMexicoHistoricalFeatures {
   }
 
   /**
+   * Filtering method for getting all historical features in a particular county.
+   * Since this class focuses on New Mexico features, the input parameter is limited.
    *
+   * @param county, the particular county whose features you want to get; not case-sensitive
+   * @return a List container holding HistoricalFeature objects which are located in
+   *         the inputted county; returns an empty List if there are no features in this county
    */
+  public List<HistoricalFeature> findFeaturesInCounty(String county) {
+    List<HistoricalFeature> featuresInCounty = new ArrayList<HistoricalFeature>();
+    for (HistoricalFeature feature : features) {
+      String featuresCounty = feature.getPropertyData().getCountyName();
+
+      if (county.equalsIgnoreCase(featuresCounty)) {
+        featuresInCounty.add(feature);
+      }
+    }
+
+    return featuresInCounty;
+  }
 
   /**
-   * Determines whether or not a Double value is roughly equal to zero
+   * Filtering method for getting all the Historical features which are at a certain distance
+   * from a particular latitude-longitude coordinate
+   *
+   * @param latitude degrees as a Double, part of the coordinate from which to search
+   * @param longitude degrees as a Double, part of the coordinate from which to search
+   * @param desiredDistance Double distance in miles from the coordinate to search for features from
+   * @return A List of HistoricalFeature objects that are within the distance
+   *         of the latitude-longitude coordinate provided
+   */
+  public List<HistoricalFeature> findNearbyFeatures(Double latitude, Double longitude, Double desiredDistance) {
+    List<HistoricalFeature> nearbyFeatures = findLocatableFeatures();
+    for (HistoricalFeature feature : features) {
+      Double milesLongitudallyAway =
+          Math.abs(feature.getPropertyData().getLongitudeAsNumber()) * MILES_PER_DEGREE_LONGITUDE;
+      Double milesLatitudallyAway =
+          Math.abs(feature.getPropertyData().getLatitudeAsNumber()) * MILES_PER_DEGREE_LATITUDE;
+      Double actualDistance =
+          Math.sqrt(milesLongitudallyAway * milesLongitudallyAway - milesLatitudallyAway * milesLatitudallyAway);
+
+      if (actualDistance < desiredDistance) {
+        nearbyFeatures.add(feature);
+      }
+    }
+
+    return nearbyFeatures;
+  }
+
+  /**
+   * Filtering method for finding the historical features that were added to the dataset
+   * between the two specified years.
+   *
+   * @param earliest, earliest bound of year to search for, inclusive
+   * @param latest, latest bound of year to search for, exclusive
+   * @return
+   */
+  public List<HistoricalFeature> findFeaturesAddedInYearRange(int earliest, int latest) {
+    List<HistoricalFeature> featuresAddedInRange = new ArrayList<HistoricalFeature>();
+    for (HistoricalFeature feature : features) {
+      String featuresAddDate = feature.getPropertyData().getDateCreated();
+
+      // Checks if date in dataset is valid
+      if (isValidDateFormat(featuresAddDate)) {
+        int year = Integer.parseInt(featuresAddDate.substring(0, 4));
+
+        if (year < latest && year >= earliest) {
+          featuresAddedInRange.add(feature);
+        }
+      }
+    }
+
+    return featuresAddedInRange;
+  }
+
+  /**
+   * Helper function to determine if a string is in the date format YYYY/MM/DD
+   *
+   * @param date, String you want to determine if it has the proper YYYY/MM/DD formatting
+   * @return True if date is valid and in that format, false if not
+   */
+  private Boolean isValidDateFormat(String date) {
+    if (date.length() != 10) {
+      return false;
+    }
+
+    try {
+      int year = Integer.parseInt(date.substring(0, 4));
+      int month = Integer.parseInt(date.substring(5, 7));
+      int day = Integer.parseInt(date.substring(8));
+
+      if (month < 1 || month > 12 || day > 31 || day < 1 || year < 0) {
+        return false;
+      }
+    } catch (Exception e) {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Helper function that determines whether or not a Double value is roughly equal to zero
    *
    * @param number, a Double for the value you want to check
-   * @return true if number is roughly equal to zero, false if not
+   * @return True if number is roughly equal to zero, False if not
    */
   private Boolean isEqualToZero(Double number) {
+
     return Math.abs(number - 0.0) < EPSILON;
   }
 
--- JSONX/src/test/com/example/JSONXTest.java
+++ JSONX/src/test/com/example/JSONXTest.java
@@ -4,6 +4,9 @@ package com.example;
 import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 // Using JSON data from the following website:
@@ -11,15 +14,141 @@ import static org.junit.Assert.assertEquals;
 public class JSONXTest {
     private Gson gson;
 
+    private static final String myJson = "{" +
+        "\"features\": [{" +
+        "    \"geometry\": {" +
+        "      \"type\": \"Point\"," +
+        "      \"coordinates\": [-107.3175179999999, 32.22536869999996]" +
+        "       }," +
+        "    \"type\": \"Feature\"," +
+        "    \"properties\": {" +
+        "      \"DATE_CREAT\": \"1993/08/01\"," +
+        "      \"PRIM_LONG1\": -107.317518," +
+        "      \"STATE_ALPH\": \"NM\"," +
+        "      \"ELEVATION\": 1286.0," +
+        "      \"PRIMARY_LA\": \"321331N\"," +
+        "      \"FEATURE_CL\": \"Post Office\"," +
+        "      \"observed\": \"\"," +
+        "      \"SOURCE_LAT\": \"\"," +
+        "      \"FEATURE_NA\": \"Cambray Post Office (historical)\"," +
+        "      \"PRIM_LONG_\": \"1071903W\"," +
+        "      \"id\": 8361479," +
+        "      \"FEATURE_ID\": 937222.0," +
+        "      \"SOURCE_L_2\": \"\"," +
+        "      \"STATE_NUME\": 35.0," +
+        "      \"MAP_NAME\": \"Cambray\"," +
+        "      \"COUNTY_NAM\": \"Luna\"," +
+        "      \"DATE_EDITE\": \"\"," +
+        "      \"SOURCE_LON\": \"\"," +
+        "      \"COUNTY_NUM\": 29.0," +
+        "      \"SOURCE_L_1\": \"\"," +
+        "      \"PRIM_LAT_D\": 32.225369" +
+        "       }" +
+        "   ]" +
+        "  }";
+
     @Before
     public void setUp() {
-        // This is run before every test, so it will reset the Gson parser every test
+        // reset the Gson parser before every test
         gson = new Gson();
     }
 
     @Test
-    public void sanityCheck() {
+    public void test_JSONStringFormat() {
+        Boolean correctJSON = true;
+        try {
+            NewMexicoHistoricalFeatures newMexicoHistoricalFeatures =
+                gson.fromJson(myJson, NewMexicoHistoricalFeatures.class);
+        } catch ( Exception e ) {
+            correctJSON = false;
+        }
+        assertEquals(true, correctJSON);
+    }
 
-        assertEquals(1,1);
+    @Test
+    public void test_findFeaturesInCountyLunaCounty() {
+        try {
+            NewMexicoHistoricalFeatures newMexicoHistoricalFeatures =
+                gson.fromJson(myJson, NewMexicoHistoricalFeatures.class);
+            List<HistoricalFeature> lunaFeatures = newMexicoHistoricalFeatures.findFeaturesInCounty("Luna");
+
+            assertEquals(lunaFeatures.size(), 57);
+
+        } catch ( Exception e ) {
+            assertEquals(true, false);
+        }
+    }
+
+    @Test
+    public void test_findFeaturesInCountyRioArribaCounty() {
+        try {
+            NewMexicoHistoricalFeatures newMexicoHistoricalFeatures =
+                gson.fromJson(myJson, NewMexicoHistoricalFeatures.class);
+            List<HistoricalFeature> rioArribaFeatures = newMexicoHistoricalFeatures.findFeaturesInCounty("Rio Arriba");
+
+            assertEquals(rioArribaFeatures.size(), 100);
+
+        } catch ( Exception e ) {
+            assertEquals(true, false);
+        }
+    }
+
+    @Test
+    public void test_findLocatableFeatures() {
+        try {
+            NewMexicoHistoricalFeatures newMexicoHistoricalFeatures =
+                gson.fromJson(myJson, NewMexicoHistoricalFeatures.class);
+            List<HistoricalFeature> locatableFeatures = newMexicoHistoricalFeatures.findLocatableFeatures();
+
+            assertEquals(locatableFeatures.size(), 795);
+
+        } catch ( Exception e ) {
+            assertEquals(true, false);
+        }
+    }
+
+    @Test
+    public void test_findFeaturesAddedInYearRange() {
+        try {
+            NewMexicoHistoricalFeatures newMexicoHistoricalFeatures =
+                gson.fromJson(myJson, NewMexicoHistoricalFeatures.class);
+            List<HistoricalFeature> featuresInRange =
+                newMexicoHistoricalFeatures.findFeaturesAddedInYearRange(1980, 1981);
+
+            assertEquals(featuresInRange.size() == 53, true);
+
+        } catch ( Exception e ) {
+            assertEquals(true, false);
+        }
+    }
+
+    @Test
+    public void test_findNearbyFeaturesClose() {
+        try {
+            NewMexicoHistoricalFeatures newMexicoHistoricalFeatures =
+                gson.fromJson(myJson, NewMexicoHistoricalFeatures.class);
+            List<HistoricalFeature> nearbyFeatures =
+                newMexicoHistoricalFeatures.findNearbyFeatures(-106.0, 35.0, 5.0);
+
+            assertEquals(nearbyFeatures.size(), 1);
+
+        } catch ( Exception e ) {
+            assertEquals(true, false);
+        }
+    }
+
+    @Test
+    public void test_findNearbyFeaturesAllKnownLocations() {
+        try {
+            NewMexicoHistoricalFeatures newMexicoHistoricalFeatures =
+                gson.fromJson(myJson, NewMexicoHistoricalFeatures.class);
+            List<HistoricalFeature> nearbyFeatures =
+                newMexicoHistoricalFeatures.findNearbyFeatures(-106.0, 35.0, 1000.0);
+
+            assertEquals(nearbyFeatures.size() == 795, true);
+
+        } catch ( Exception e ) {
+            assertEquals(true, false);
+        }
     }
 }
